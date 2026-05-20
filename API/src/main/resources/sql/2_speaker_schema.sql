c78c4884fbff114540a88fdafbfaf376807dc202
MarcoRazafy <hei.marco.10@gmail.com>
2026-05-20 13:59:06 +0300

    feat(room): add room API and link sessions to rooms

--- API/src/main/resources/sql/2_speaker_schema.sql
+++ API/src/main/resources/sql/2_speaker_schema.sql
@@ -5,7 +5,7 @@ ALTER TABLE   speaker drop column link;
 
 
 --- mamafa anze données existant ao anaty base aloha
-TRUNCATE TABLE public.speaker RESTART IDENTITY;
+TRUNCATE TABLE public.room RESTART IDENTITY;
 ---zay vao manao anito alter table ito
 ALTER TABLE public.speaker
     ADD COLUMN initials VARCHAR(100) NOT NULL;
