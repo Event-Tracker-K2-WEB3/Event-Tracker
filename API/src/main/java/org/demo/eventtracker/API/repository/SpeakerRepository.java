1358f9712c7559db80dbca7d6e33f8658a012291
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-05-22 13:32:08 +0300

    fix: sessionCount display in SpeakersPage

--- API/src/main/java/org/demo/eventtracker/API/repository/SpeakerRepository.java
+++ API/src/main/java/org/demo/eventtracker/API/repository/SpeakerRepository.java
@@ -2,9 +2,18 @@ package org.demo.eventtracker.API.repository;
 
 import org.demo.eventtracker.API.entity.Speaker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface SpeakerRepository extends JpaRepository<Speaker, Integer> {
     List<Speaker> findAllByOrderByNameAsc();
+
+    @Query(value = """
+        SELECT COUNT(*)
+        FROM session_speaker
+        WHERE speaker_id = :speakerId
+        """, nativeQuery = true)
+    Integer countSessionsBySpeakerId(@Param("speakerId") Integer speakerId);
 }
